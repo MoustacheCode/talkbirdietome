88c94dda38bb5fc98af520ef50fa9398131effe6
Richard <r-duerden@live.co.uk>
2026-07-28 10:39:27 +0100

    Added comments to fully understand the process when I return later

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,18 @@
+// Imports Hapi and dotenv modules
 import Hapi from "@hapi/hapi";
 import dotenv from "dotenv";
 
+// Loads variables from .env
 dotenv.config();
 
+// Starts server and sets health route
 const init = async () => {
     const server = Hapi.server({
         port: 3000,
         host: "localhost",
     });
 
+    // Health check endpoint to see if server is running
     server.route({
         method: "GET",
         path: "/health",
@@ -16,9 +20,9 @@ const init = async () => {
             return { status: "ok" };
         },
     });
-
+    // Starts server and console log to show server is running
     await server.start();
     console.log(`Server running at: ${server.info.uri}`);
 };
-
+// Calls init function to start the server
 init();
