67731c679e4d230b2f7fa5beb92532bb4f5d0742
Richard <r-duerden@live.co.uk>
2026-07-29 15:04:13 +0100

    fix prisma client issue of missing adapter - server now runs

--- backend/src/prisma/client.js
+++ backend/src/prisma/client.js
@@ -1,7 +1,14 @@
 import pkg from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+
 const { PrismaClient } = pkg;
 
+const adapter = new PrismaPg({
+    // Connection string to the PostgreSQL database
+    connectionString: process.env.DATABASE_URL,
+});
+
 // Creates a new instance of the PrismaClient
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({ adapter });
 
 export default prisma; // Exports the prisma client so it can be used in other files
