b79ce92cb9ef31d7cbbb4573026766237f3ee56c
Richard <r-duerden@live.co.uk>
2026-07-29 12:19:43 +0100

    created a roundcontroller

--- backend/src/controllers/roundController.js
+++ backend/src/controllers/roundController.js
@@ -1,1 +1,13 @@
 import { roundService } from "../services/roundService.js";
+
+export const roundController = {
+    getRounds: async (req, res) => {
+        const rounds = await roundService.getAllRounds();
+        res.json(rounds); // Send the rounds as a JSON repsonse
+    },
+
+    createRound: async (req, res) => {
+        const newRound = await roundService.createRound(req.body);
+        res.json(newRound); // Send new round as JSON response
+    },
+};
