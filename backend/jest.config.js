27c0613b28c6572e1d3ca0b383cf80d5b75ed971
Richard <r-duerden@live.co.uk>
2026-07-30 15:52:27 +0100

    downgraded Jest to v29 for native esm support without the need of a transformer

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,5 +1,3 @@
 export default {
     testEnvironment: "node", // Specifies the test environment as Node.js
-    transform: {},
-    extensionsToTreatAsEsm: [".js"], // treats .js files as ES modules
 };
