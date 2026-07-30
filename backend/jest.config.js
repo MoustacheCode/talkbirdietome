851b9ce34fcdad27fbf79623f5f1bf77f145cb29
Richard <r-duerden@live.co.uk>
2026-07-30 15:38:29 +0100

    added extensionsToTreatAsEsm to fix import error

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,4 +1,5 @@
 export default {
     testEnvironment: "node", // Specifies the test environment as Node.js
     transform: {},
+    extensionsToTreatAsEsm: [".js"], // treats .js files as ES modules
 };
