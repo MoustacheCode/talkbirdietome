0c1df62180c4fc12d48838f1d67c866d56e66d5e
Richard <r-duerden@live.co.uk>
2026-08-07 16:43:10 +0100

    refactored server.js so server creation is top-level and exported for jest tests

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,36 +8,34 @@ import roundRoutes from "./routes/roundRoutes.js";
 dotenv.config();
 
 // Starts server and sets health route
-const init = async () => {
-    const server = Hapi.server({
-        port: process.env.PORT || 8080,
-        host: "0.0.0.0",
-        routes: {
-            payload: {
-                parse: true,
-                allow: "application/json",
-            },
+export const server = Hapi.server({
+    port: process.env.PORT || 8080,
+    host: "0.0.0.0",
+    routes: {
+        payload: {
+            parse: true,
+            allow: "application/json",
         },
-    });
+    },
+});
 
-    // Health check endpoint to see if server is running
-    server.route({
-        method: "GET",
-        path: "/health",
-        handler: () => {
-            return { status: "ok" };
-        },
-    });
+// Health check endpoint to see if server is running
+server.route({
+    method: "GET",
+    path: "/health",
+    handler: () => {
+        return { status: "ok" };
+    },
+});
 
-    // Adds the round routes to the server
-    server.route(roundRoutes);
+// Adds the round routes to the server
+server.route(roundRoutes);
 
-    // Starts server and console log to show server is running unless in test environment
-    if (process.env.NODE_ENV !== "test") {
-        const start = async () => {
-            await server.start();
-            console.log(`Server running at: ${server.info.uri}`);
-        };
-        start();
-    }
-};
+// Starts server and console log to show server is running unless in test environment
+if (process.env.NODE_ENV !== "test") {
+    const start = async () => {
+        await server.start();
+        console.log(`Server running at: ${server.info.uri}`);
+    };
+    start();
+}
